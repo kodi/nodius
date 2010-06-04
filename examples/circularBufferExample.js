0bff3660cf161a3edaa6c7b795cf54a5326df3eb
Dragan Bajcic <dragan.bajcic@gmail.com>
2010-06-04 07:23:24 +0200

    added circular buffer example

--- examples/circularBufferExample.js
+++ examples/circularBufferExample.js
@@ -1,6 +1,6 @@
 require.paths.unshift(__dirname+'/../lib/');
-
-var echo = require('sys').puts;
+var sys = require('sys');
+var echo = sys.puts;
 var CircularBuffer = require('CircularBuffer').CircularBuffer;
 var b = new CircularBuffer(4);
 
@@ -14,13 +14,35 @@ b.getEach(function(element){
     echo(element);    
 });
 
-/*
-OUTPUT:
+/* OUTPUT:
 
 $>  node circularBufferExample.js
 e
 d
 c
 b
-$>
- */
+$>   */
+
+
+// STORING OBJECTS
+
+var b1 = new CircularBuffer(4);
+
+b1.push({a:1,b:"a"});
+b1.push({a:2,b:"b"});
+b1.push({a:3,b:"c"});
+b1.push({a:4,b:"d"});
+
+b1.getEach(function(element){
+    echo(sys.inspect(element));
+});
+
+
+/* OUTPUT:
+
+{ a: 4, b: 'd' }
+{ a: 3, b: 'c' }
+{ a: 2, b: 'b' }
+{ a: 1, b: 'a' }
+
+*/
