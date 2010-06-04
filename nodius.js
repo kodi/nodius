8469f2fb36ad6add9cc88a99eaa96981b48fee23
Dragan Bajcic <dragan.bajcic@gmail.com>
2010-06-04 11:36:22 +0200

    cleanup

--- nodius.js
+++ nodius.js
@@ -2,12 +2,4 @@ require.paths.unshift(__dirname+'/lib/');
 var logger = require('sys').log;
 var echo = require('sys').puts;
 var CircularBuffer = require('CircularBuffer').CircularBuffer;
-var b = new CircularBuffer(4);
 
-b.push("a");
-b.push("b");
-b.push("c");
-b.push("d");
-b.push("e");
-
-b.each
