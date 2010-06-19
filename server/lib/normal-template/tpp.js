ddca37063680941846e45cd456e0bab7e22cb8f5
Dragan Bajcic <dragan.bajcic@gmail.com>
2010-06-19 05:52:08 +0200

    added missing files

---
+++ server/lib/normal-template/tpp.js
@@ -0,0 +1,59 @@
+/**
+ * Normal Template Pre Processor.
+ *
+ * Performs static pre processing of templates:
+ * - static fragment inclusion.
+ * - static expansion of 'meta' templates (template of templates).
+ */
+
+var TEMPLATE_RE = /\{\#(template|t) (.*?)\}/;
+
+/**
+ * Define the 'meta' template.
+ * Example:
+ *   {#template /path/to/layout.html}
+ */
+exports.getTemplatePath = function(str) {
+    var match = str.match(TEMPLATE_RE);
+    if (match) {        
+        return match[2];
+    } else {
+        return false;
+    }
+}
+
+var INCLUDE_RE = /\{#include (.*?)\}/g;
+
+/**
+ * (Recursively) Expand include directives.
+ * Example:
+ *   {#include /path/to/fragment}
+ */
+exports.expandIncludes = function(str, load, options) {
+    while (INCLUDE_RE.test(str)) {
+        str =  str.replace(INCLUDE_RE, function(match, path) {
+            return load(path, options)/* || "error: cannot include '" + path + "'";        */
+        });
+    }
+    
+    return str;
+}
+
+var BLOCK_RE = /\{\#(def|define|d) (.*?)\}([\s\S]*?)\{\/\#(def|define|d)(.*?)\}/g;
+
+/**
+ * Extract data to be interpolated in the parent template.
+ * Example:
+ *   {#def content}
+ *   ..
+ *   {/#def}
+ */
+exports.extractData = function(str) {
+    var data = {};
+    
+    var str = str.replace(BLOCK_RE, function(match, tag, key, value) {
+        data[key] = value;
+    });
+    
+    return data;
+}
